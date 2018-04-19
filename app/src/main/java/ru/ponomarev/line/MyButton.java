ef3be5ccb2b9fa27b12d3a50f35e027bed33a08b
Nikita Ponomarev <iponomar47@gmail.com>
2018-04-19 11:54:51 +0800

    Initial commit

---
+++ app/src/main/java/ru/ponomarev/line/MyButton.java
@@ -0,0 +1,58 @@
+package ru.ponomarev.line;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+public class MyButton extends AppCompatButton {
+
+    private MyOnClickListener mClickListener;//наш интерфейс учета кликов для MainActivity
+    int idX = 0;
+    int idY = 0;
+
+    //конструктор, в котором будем задавать координаты кнопки в матрице
+    public MyButton(Context context, int x, int y) {
+        super(context);
+        idX = x;
+        idY = y;
+    }
+
+    public MyButton(Context context) {
+        super(context);
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override //метод View для отлавливания кликов
+    public boolean performClick() {
+        super.performClick();
+
+        mClickListener.OnTouchDigit(this);//будем дергать метод интерфейса
+        return true;
+    }
+
+    public void setOnClickListener(MyOnClickListener listener){
+        mClickListener = listener;
+    }
+
+    public int getIdX(){
+        return idX;
+    }
+
+    public int getIdY(){
+        return idY;
+    }
+
+
+    //Интерфейс для MainActivity
+    public interface MyOnClickListener {
+
+        void OnTouchDigit(MyButton v);
+    }
+}
